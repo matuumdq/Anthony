c811622ea1a7751c0de20aed8998b239b56006bd
matuumdq <salinasmateo@outlook.com>
2023-05-09 17:01:25 -0300

    feat: :zap: change wpp

--- src/db.js
+++ src/db.js
@@ -4,27 +4,27 @@ export const cursos = [
       name: "Taller",
       explanation: 'Para las personas que quieren profundizar y mejorar ciertos aspectos del italiano como la escritura, la lectura y la conversación',
       img: "https://images.pexels.com/photos/7522/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      msg: 'https://api.whatsapp.com/send?phone=+5492236681270&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Taller'
+      msg: 'https://api.whatsapp.com/send?phone=+5491123908904&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Taller'
     },
     {
       id: 1,
       name: "Turista",
       explanation: 'Para personas que quieren ir a visitar Italia y quieren aprender las cosas esenciales para poder moverse con una base lingüística',
       img: "https://images.pexels.com/photos/733830/pexels-photo-733830.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      msg: 'https://api.whatsapp.com/send?phone=+5492236681270&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Turista'
+      msg: 'https://api.whatsapp.com/send?phone=+5491123908904&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Turista'
     },
     {
       id: 2,
       name: "Empresarial",
       explanation: 'Para personas que quieren estudiar italiano y así ofrecer mejores servicios dentro de una impresa con ciertas necesidades.',
       img: "https://images.pexels.com/photos/15164659/pexels-photo-15164659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      msg: 'https://api.whatsapp.com/send?phone=+5492236681270&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Empresarial'
+      msg: 'https://api.whatsapp.com/send?phone=+5491123908904&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Empresarial'
     },
     {
       id: 3,
       name: "Fluido",
       explanation: 'Para personas que tienen el plan de estudiar el idioma italiano de manera gradual y progresiva con el objetivo de alcanzar niveles solidos en el manejo del idioma.',
       img: "https://images.pexels.com/photos/2064827/pexels-photo-2064827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      msg: 'https://api.whatsapp.com/send?phone=+5492236681270&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Fluido'
+      msg: 'https://api.whatsapp.com/send?phone=+5491123908904&text=Hola,%20estoy%20interesado%20en%20el%20curso%20de%20Fluido'
     }
   ];
