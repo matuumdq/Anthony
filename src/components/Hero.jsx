68503112eee46231b1ca429ffacdec254fc3769a
Damian Barera <damian_g1988@hotmail.com>
2023-06-06 00:44:04 -0300

    oscurecer titulo de saludo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ const Hero = () => {
   return (
     <div className="max-w-full h-screen bg-[url('https://upload.wikimedia.org/wikipedia/commons/a/a2/Piazza_Venezia_-_Il_Vittoriano.jpg')] bg-cover">
         <div className=" max-w-full h-full flex flex-col justify-center mx-5 md:mx-20 gap-4">
-            <h1 className="text-4xl text-teal-500 font-semibold w-full ">CIAO!<strong> Sono Anthony</strong></h1>
-            <p className="font-semibold text-teal-600">Tu profesor de italiano </p>
+            <h1 className="text-4xl text-teal-900 font-semibold w-full ">CIAO!<strong> Sono Anthony</strong></h1>
+            <p className="font-semibold text-teal-900">Tu profesor de italiano Nativo </p>
             <a href="#form" className="font-light bg-emerald-500 shadow-xl mx-auto ml-0 px-4 py-2 rounded-md hover:scale-105 ease-in duration-300 text-white">Contactame </a>
 
         </div>
